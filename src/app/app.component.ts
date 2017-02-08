85b0d274ba4647d80d7fe70cae5c8a907cfd0d09
José Pedro de Sousa Martins <zpedro.martins@gmail.com>
2017-02-08 01:03:33 +0000

    Working Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Planets, PlanetsService } from './shared';
 
 @Component({
   selector: 'Planets-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Planets works!';
+export class AppComponent implements OnInit {
+
+  planetList: Planets[] = [];
+  selectedPlanet: Planets;
+
+  constructor(private planetService: PlanetsService) { }
+  
+  ngOnInit() {
+    this.planetService.getPlanets().then(planets => this.planetList = planets);
+  }
+
+  showPlanetInfo(selPlanet) {
+    this.selectedPlanet = selPlanet;
+  }
 }
